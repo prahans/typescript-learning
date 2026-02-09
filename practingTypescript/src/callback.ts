c6b363b1c27f444ea722315faa6675fb76ff7f46
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-02-09 17:28:50 +0545

    learning js with ts

---
+++ practingTypescript/src/callback.ts
@@ -0,0 +1,186 @@
+function one(){
+    return 1;
+}
+
+function two(){
+    return one() + one();
+}
+
+function three(){
+    console.log(one() + two());
+}
+
+three();
+
+
+let h1 = document.querySelector("h1") as HTMLElement;
+
+// function changeColor(color: string, delay: number, nextColorChange?: () => void){
+//     setTimeout(() => {
+//     h1.style.color = color;
+//     if(nextColorChange) nextColorChange();
+// }, delay);
+// }
+
+// changeColor("red", 1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000, () => {
+//             changeColor("blue", 1000, () => {
+//                 changeColor("pink", 1000);
+//             })
+//         })
+//     })
+// })
+
+
+// function savetoDb(data: string, success: () => void, failure: () => void): void{
+//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//     if(internetSpeed > 4){
+//        success();
+//     }else{
+//         failure();
+//     }
+// }
+
+// savetoDb(
+//     "anurag",
+//     () => {
+//         console.log("data1 was saved.");
+//         savetoDb("anurag", () => {
+//             console.log("data2 was saved.");
+//             savetoDb("javaScript", () => {
+//                 console.log("data3 was saved");
+//                 savetoDb("typescript", () => {
+//                     console.log("data4 was saved");
+//                     savetoDb("react", () => {
+//                         console.log("data5 was saved");
+//                         savetoDb("node js", () => {
+//                             console.log("data6 was saved");
+//                             savetoDb("express js", () => {
+//                                 console.log("data7 was saved");
+//                                 savetoDb("mongoDb", () => {
+//                                     console.log("data8 was saved");
+//                                     savetoDb("postgresql", () => {
+//                                         console.log("data9 was saved");
+//                                     }, () => {
+//                                         console.log("weak connection. data not saved");
+//                                     })
+//                                 }, () => {
+//                                     console.log("weak connection. data not saved");
+//                                 })
+//                             }, () => {
+//                                 console.log("weak connection. data not saved");
+//                             })
+//                         }, () => {
+//                             console.log("weak connection. data not saved");
+//                         })
+//                     }, () => {
+//                         console.log("weak connection. data not saved");
+//                     })
+//                 }, () => {
+//                     console.log("weak connection. data not saved");
+//                 })
+//             }, () => {
+//                 console.log("weak connection. data not saved");
+//             })
+//         }, () => {
+//             console.log("weak connection. data not saved");
+//         })
+//     },
+//     () => {
+//         console.log("weak connection. data not saved");
+//     }
+// )
+
+
+function savetoDb(data: string){
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if(internetSpeed > 4){
+            resolve("success : data was saved");
+        }else {
+            reject("failure : weak connection");
+        }
+    });
+}
+
+savetoDb("anurag")
+.then((result) => {
+    console.log(result);
+    console.log("data saved");
+    return savetoDb("javaScript");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data2 saved");
+    return savetoDb("typescript");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data3 saved");
+    return savetoDb("react");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data4 saved");
+    return savetoDb("node js");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data5 saved");
+    return savetoDb("express js");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data6 saved");
+    return savetoDb("mongoDB");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data7 saved");
+    return savetoDb("postgresql");
+})
+.then((result) => {
+    console.log(result);
+    console.log("data8 saved");
+})
+.catch((error) => {
+    console.log(error);
+    console.log("promis is rejected");
+})
+ 
+ 
+// refactoring old code
+
+function changeColor(color: string, delay: number){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        h1.style.color = color;
+        resolve(`color changed to ${color}`);
+    }, delay);
+});
+}
+
+changeColor("red", 1000)
+.then((result) => {
+    console.log(result);
+    return changeColor("orange", 1000);
+})
+.then((result) => {
+    console.log(result);
+    return changeColor("green", 1000);
+})
+.then((result) => {
+    console.log(result);
+    return changeColor("blue", 1000);
+})
+.then((result) => {
+    console.log(result);
+    return changeColor("pink", 1000);
+})
+.then((result) => {
+    console.log(result);
+    return changeColor("purple", 1000);
+}).then((result) => {
+    console.log(result);
+})
