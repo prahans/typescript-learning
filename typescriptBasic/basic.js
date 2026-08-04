71c31bc409798201d89dd68f556157bbf5f1f489
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-04 15:50:19 +0545

    learning typescript

--- typescriptBasic/basic.js
+++ typescriptBasic/basic.js
@@ -1,4 +1,12 @@
 let userName;
+let userAge = 38;
 userName = "John Doe";
 console.log(typeof userName); // Output: string
+// userAge = "38"; // This will cause a TypeScript error because userAge is declared as a number
+console.log(typeof userAge); // Output: number
+function add(a, b = 5) {
+    return a + b;
+}
+add(10); // output: 15
+add(10, 20); // output: 30
 export {};
--- typescriptBasic/basic.ts
+++ typescriptBasic/basic.ts
@@ -6,4 +6,22 @@ console.log(typeof userName); // Output: string
 // userAge = "38"; // This will cause a TypeScript error because userAge is declared as a number
 console.log(typeof userAge); // Output: number
 
+function add(a: number, b = 5) {
+  return a + b;
+}
+
+add(10); // output: 15
+add(10, 20); // output: 30
+// add("10"); // This will cause a TypeScript error because the first argument is not a number
+// add(10, '20'); // This will cause a TypeScript error because the second argument is not a number
+
+// understand union types
+let age: number | string = 37;
+
+age = "38"; // this is valid because age can be a string
+
+let hobbines = ["Reading", "Traveling", "Cooking"];
+// let hobbines: string[] = ["Reading", "Traveling", "Cooking"];
+// hobbines.push(10); // this will cause a typescript error because hobbines is an array of strings
+
 export {};
