c1ea65c73f716874c8cf31581be2e73bce818e8e
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-06 17:46:58 +0545

    learning generics types in ts

---
+++ generics.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ["spiderman", "iron man"];
+let store = {};
+store.name = "max";
+store.isInstructor = true;
+let nameStore = {};
+// nameStore.id = 134;  // Type 'number' is not assignable to type 'string'.
--- generics.ts
+++ generics.ts
@@ -1,1 +1,12 @@
 let names: Array<string> = ["spiderman", "iron man"];
+
+type DataStore<T> = {
+  [key: string]: T;
+};
+
+let store: DataStore<string | boolean> = {};
+store.name = "max";
+store.isInstructor = true;
+
+let nameStore: DataStore<string> = {};
+// nameStore.id = 134;  // Type 'number' is not assignable to type 'string'.
