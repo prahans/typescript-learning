b8ed6959158040292570de0ae2d780ca951cea31
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-06 08:04:37 +0545

    learning advance typescript

--- typescriptBasic/advance.ts
+++ typescriptBasic/advance.ts
@@ -51,3 +51,18 @@ interface Status {
 
 interface AccessFileData extends FileData, Status {}
 interface AccessDatabaseData extends DatabaseData, Status {}
+
+type FileSource = { path: string };
+const fileSource: FileSource = { path: "/some/path/to/file.csv" };
+
+type DBSource = { connectionUrl: string };
+const dbSource: DBSource = { connectionUrl: "connection-url" };
+
+type Source = FileData | DBSource;
+function loadData(source: Source) {
+  if ("path" in source) {
+    // source.path;
+    return;
+  }
+  //   source.connectionUrl;
+}
